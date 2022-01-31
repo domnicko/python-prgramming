1c9b3c72381ef56274bf77b048710ca193ca4a48
domnicko <97869824+domnicko@users.noreply.github.com>
2022-01-31 10:31:18 +0300

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,6 @@
+amount=int(input("Enter sale Amount"))
+if amount>1000:
+ discount= amount*0.05
+ print(discount)
+else:
+ print(amount)
---
+++ vote.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age"))
+citizenship=(input("Enter citizenship"))
+if citizenship=="kenyan" and age>=18:
+    print ("eligible to vote")
+else:
+        print("not eligible to vote")
+    
